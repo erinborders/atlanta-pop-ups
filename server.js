d618286ee6fdf4f6c510ffc06d46f8ee5d111c5e
Erin Borders <borders16@gmail.com>
2019-07-03 13:20:04 -0400

    made a redirect from root route to my locations router path

--- server.js
+++ server.js
@@ -61,9 +61,14 @@ app.set('view engine', 'hbs')
  * add router for the application to use. The first argument is a prefix to all
  * the paths defined in the router.
  */
+
 app.use('/locations', locationRouter)
 app.use('/locations/:locationId/food', foodRouter)
 app.use('/locations/:locationId/shops', shopRouter)
+
+app.get('/', (req, res) => {
+    res.redirect('/locations')
+})
 /* Step 5
  *
  * Set the port the server is to run on
