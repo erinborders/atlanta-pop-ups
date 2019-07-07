90a9179cb54edcc2cf3f722bac8d362cf64e456f
Erin Borders <borders16@gmail.com>
2019-07-07 17:44:53 -0400

    commit before debugging test data file

--- models/testData.js
+++ models/testData.js
@@ -40,23 +40,60 @@ const foodShoppe = {
     time: "7/1/19"
 }
 
+//shop pop ups
+const modernMystic = {
+    shop: "Modern Mystic",
+    aesthetic: "New Age Witch",
+    product: "Mystical items",
+    price: "$$",
+    address: "Ponce City Market",
+    time: "6/30/19"
+}
+
+const lululemon = {
+    shop: "Lululemon Athletica",
+    aesthetic: "Athletic",
+    product: "Athletic wear",
+    price: "$$$",
+    address: "102 West Paces Ferry Rd \n Atlanta, GA 30305",
+    time: "6/29/19"
+}
+
+const executiveShop = {
+    shop: "The Executive Shop",
+    product: "Mens hats",
+    price: "$$",
+    address: "56 Walton St \n Atlanta, GA 30303",
+    time: "6/28/19"
+}
+
 locationApi.deleteAllLocations()
     .then(() => foodApi.deleteAllFood())
     .then(() => shopApi.deleteAllShops())
     .then(() => locationApi.addLocation(buckhead))
     .then((buckhead) => {
         anotherBrokenEgg.locationId = buckhead._id
-        foodApi.addFood(anotherBrokenEgg)
+        lululemon.locationId = buckhead._id
+        let brokenEgg = foodApi.addFood(anotherBrokenEgg)
+        let lulu = shopApi.addShop(lululemon)
+        Promise.all([brokenEgg, lulu])
     })
     .then(() => locationApi.addLocation(midtown))
     .then((midtown) => {
         papisCubanGrill.locationId = midtown._id
-        foodApi.addFood(papisCubanGrill)
+        modernMystic.locationId = midtown._id
+        let papis = foodApi.addFood(papisCubanGrill)
+        let mystic = shopApi.addShop(modernMystic)
+        Promise.all([papis, mystic])
     })
     .then(() => locationApi.addLocation(downtown))
     .then((downtown) => {
+        console.log(downtown)
         foodShoppe.locationId = downtown._id
-        foodApi.addFood(foodShoppe)
+        executiveShop.locationId = downtown._id
+        let shoppe = foodApi.addFood(foodShoppe)
+        let execShop = shopApi.addShop(executiveShop)
+        Promise.all([shoppe, execShop])
     })
     .then(() => {
         process.exit()
